34f9a144679cb3ae1fd62590123b06edff1123e7
Calixto <fcalixto318@gmail.com>
2025-02-11 11:36:26 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import {HashRouter as Router,Route,Routes,} from "react-router-dom";
-import HomePage from "./Pages/HomePage";
-import './app.css'
-import Nopage from "./Pages/NoPage/Nopage";
+// import {HashRouter as Router,Route,Routes,} from "react-router-dom";
+// import HomePage from "./Pages/HomePage";
+// import './app.css'
+// import Nopage from "./Pages/NoPage/Nopage";
 
 function App() {
 
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="*" element={<Nopage/>} />
-      </Routes>
-    </Router>
-
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<HomePage />} />
+    //     <Route path="*" element={<Nopage/>} />
+    //   </Routes>
+    // </Router>
+<div>
+  <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere possimus itaque odit doloribus labore laudantium, hic incidunt perspiciatis pariatur ad non minus dolores minima ullam suscipit animi placeat cumque fuga?</h1>
+</div>
   )
 }
 
