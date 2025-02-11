4be3362182a190174a510678f593afbfc45477f8
Calixto <fcalixto318@gmail.com>
2025-02-11 09:18:06 -0300

    ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router,Route,Routes,} from "react-router-dom";
+import {HashRouter as Router,Route,Routes,} from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import './app.css'
 import Nopage from "./Pages/NoPage/Nopage";
